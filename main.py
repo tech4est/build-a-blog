54fa7f66affa0085f640315f4a279a995fafd075
Aaron Mattix <lmattix@gmail.com>
2017-10-06 22:00:10 -0500

    Configured app to access database

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:buildablog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+#stopping point; added all the proper info in database connection string above
 
 tasks = []
 
